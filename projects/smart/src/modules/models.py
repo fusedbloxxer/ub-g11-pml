7722046b737e6954d03e2fc86e81b35d27ffde6d
fusedbloxxer <voidwarden@yahoo.com>
2022-11-28 03:22:52 +0200

    Refactor code for Model class. => Adapt. Improve. Overcome.

---
+++ projects/smart/src/modules/models.py
@@ -0,0 +1,316 @@
+from __future__ import annotations
+from typing import TypedDict, TypeVar, Callable, Generic, List, Any, Tuple
+import abc
+import pathlib as pb
+from sklearn.svm import SVC
+from nvitop.callbacks.keras import GpuStatsLogger
+from sklearn.metrics import confusion_matrix
+from matplotlib import pyplot as pt
+import numpy as ny
+import tensorflow as tw
+
+
+class HyperParams(TypedDict):
+    pass
+
+
+class TrainParams(TypedDict):
+    n_folds: int
+
+
+class TCNNParams(HyperParams):
+    optim: Callable[[], tw.keras.optimizers.Optimizer]
+    m_init: tw.keras.initializers.VarianceScaling
+    activ_fn: tw.keras.layers.Activation
+    lr_update: Tuple[int, int, float]
+    n_filters: int
+    dropout: float
+    n_units: int
+    n_batch: int
+    n_epochs: int
+
+
+class SVMParams(HyperParams):
+    C: float
+    kernel: str
+    gamma: str
+
+
+class HistoryProgress(abc.ABC):
+    def __init__(self):
+        pass
+
+    @abc.abstractmethod
+    def show(self) -> None:
+        """Display to the user various statistics."""
+        raise NotImplementedError()
+
+    @property
+    @abc.abstractmethod
+    def accuracy(self):
+        """Display the last train and/or valid accuracy value."""
+        raise NotImplementedError()
+
+class TrainValidHistoryProgress(HistoryProgress):
+    def __init__(self, train_accuracy, valid_accuracy = None):
+        super().__init__()
+        self.train_accuracy = train_accuracy
+        self.valid_accuracy = valid_accuracy
+
+    def show(self) -> None:
+        """Display to the user various statistics."""
+        print(f'Train Accuracy: {self.train_accuracy}')
+        if self.valid_accuracy:
+          print(f'Valid Accuracy: {self.valid_accuracy}')
+
+    @property
+    def accuracy(self):
+        """Display the last train and/or valid accuracy value."""
+        return self.train_accuracy, self.valid_accuracy
+
+class NNHistoryProgress(HistoryProgress):
+    def __init__(self, history: Any):
+        super().__init__()
+        self.history = history
+        self.has_valid = 'val_loss' in self.history
+
+    def show(self) -> None:
+        """Show a plot containing the loss and accuracy for training and/or validation."""
+        f, ax = pt.subplots(1, 2)
+        f.tight_layout()
+        for i, metric in enumerate(('categorical_accuracy', 'loss')):
+            ax[i].grid(True)
+            ax[i].set_xlabel('Epoch')
+            ax[i].set_ylabel(metric.replace('_', ' ').capitalize())
+            ax[i].plot(ny.arange(len(self.history[metric])), self.history[metric], label='train')
+        if self.has_valid:
+            ax[0].plot(ny.arange(len(self.history['val_loss'])), self.history['val_categorical_accuracy'], label='valid')
+            ax[0].legend()
+            ax[1].plot(ny.arange(len(self.history['val_loss'])), self.history['val_loss'], label='valid')
+            ax[1].legend()
+        pt.show()
+
+    @property
+    def accuracy(self):
+        """Return the last train and/or valid accuracy value."""
+        return self.history['categorical_accuracy'][-1], \
+               self.history['val_categorical_accuracy'][-1] if self.has_valid else None
+
+
+HP = TypeVar('HP', bound=HyperParams)
+
+
+class Model(abc.ABC, Generic[HP]):
+    hparams: HP
+
+    def __init__(self, hparams: HP):
+        super().__init__()
+        self.hparams = hparams
+
+    @abc.abstractmethod
+    def fit(self, train_data: ny.ndarray, train_labels: ny.ndarray,
+                  valid_data: ny.ndarray = None, valid_labels: ny.ndarray = None) -> HistoryProgress:
+        """Train the model over the given data and labels and optionally predict over time,
+        or at the end, on the validation set.
+        Args:
+            train_data: (n_samples, n_features)
+            train_labels: (n_samples, 1)
+            valid_data: (n_samples, n_features)
+            valid_labels: (n_samples, 1)
+        Returns: A history object containing statistics obtained throughout the training
+        and validation.
+        """
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def predict(self, data: ny.ndarray) -> ny.ndarray:
+        """Predict an array of labels for the given data."""
+        raise NotImplementedError()
+
+    def accuracy(self, data: ny.ndarray, labels_t: ny.ndarray) -> float:
+        """Compute the accuracy for the given data against the true labels."""
+        labels_p = self.predict(data)
+        return ny.mean(labels_p == labels_t)
+
+    def conf(self, data: ny.ndarray, labels_t: ny.ndarray) -> ny.ndarray:
+        """Compute the confusion matrix for the given data against the true labels."""
+        labels_p = self.predict(data)
+        return confusion_matrix(labels_t, labels_p)
+
+
+class SVMModel(Model[SVMParams]):
+    def __init__(self, hparams: SVMParams):
+        super().__init__(hparams)
+
+        # Instantiate the model
+        self.model_ = SVC(C=hparams['C'], kernel=hparams['kernel'],
+                          gamma=hparams['gamma'])
+
+    def fit(self, train_data: ny.ndarray, train_labels: ny.ndarray,
+                  valid_data: ny.ndarray = None, valid_labels: ny.ndarray = None) -> HistoryProgress:
+        # Train the model on the available data
+        self.model_.fit(train_data, train_labels)
+        
+        
+        # if valid_data is not None and valid_labels is not None:
+
+
+    def predict(self, data: ny.ndarray) -> ny.ndarray:
+        """Predict an array of labels for the given data."""
+        raise NotImplementedError()
+
+
+class TCNNModel(Model[TCNNParams]):
+    def __init__(self, hparams: TCNNParams, root: pb.Path = None):
+        super().__init__(hparams)
+
+        # Instantiate the model
+        self.model_ = self.__create_model()
+        self.logs_dir_ = root / 'logs'
+        self.n_workers = 8
+
+    def fit(self, train_data: ny.ndarray, train_labels: ny.ndarray,
+                  valid_data: ny.ndarray = None, valid_labels: ny.ndarray = None) -> HistoryProgress:
+        # Reshape data to proper format
+        if valid_data is not None and valid_labels is not None:
+            valid_labels = ny.array(valid_labels) - 1 # [1, 20] -> [0, 19]
+            valid_labels = tw.raw_ops.OneHot(indices=valid_labels, depth=20, on_value=1.0, off_value=0.0)
+            validation_data = valid_data.reshape((-1, 1, 150, 3)), valid_labels
+        else:
+            validation_data = None
+        train_data = train_data.reshape((-1, 1, 150, 3))
+
+        # Map labels to [0, 19] and encode them using one-hot
+        train_labels = ny.array(train_labels) - 1
+        train_labels = tw.raw_ops.OneHot(indices=train_labels, depth=20, on_value=1.0, off_value=0.0)
+
+        # Train the model and validate it on each step
+        fit_history = self.model_.fit(train_data, train_labels,
+                        validation_data=validation_data,
+                        epochs=self.hparams['n_epochs'],
+                        batch_size=self.hparams['n_batch'],
+                        workers=self.n_workers, use_multiprocessing=True,
+                        initial_epoch=0, callbacks=self.fitting_callbacks())
+
+        # Sends stats to be plotted later
+        return NNHistoryProgress(fit_history.history)
+
+    def predict(self, data: ny.ndarray) -> ny.ndarray:
+        """Predict an array of labels for the given data."""
+        probs = self.model_.predict(data.reshape(-1, 1, 150, 3),
+                            self.hparams['n_batch'],
+                            workers=self.n_workers, use_multiprocessing=True)
+        return ny.argmax(probs, axis=1) + 1 # [0, 19] -> [1, 20]
+
+    def __create_model(self) -> tw.keras.Model:
+        model_tcnn = tw.keras.Sequential([
+          tw.keras.layers.InputLayer(input_shape=(1, 150, 3)),
+          # --- First Convolutional Block ---
+          tw.keras.layers.Conv2D(self.hparams['n_filters'],
+                                 kernel_size=(1, self.hparams['s_filters']),
+                                 padding='same',
+                                 kernel_initializer=self.hparams['m_init']),
+          tw.keras.layers.BatchNormalization(),
+          self.hparams['activ_fn'],
+          tw.keras.layers.Conv2D(self.hparams['n_filters'],
+                                 kernel_size=(1, self.hparams['s_filters']),
+                                 padding='same',
+                                 kernel_initializer=self.hparams['m_init']),
+          tw.keras.layers.BatchNormalization(),
+          self.hparams['activ_fn'],
+          tw.keras.layers.MaxPool2D(pool_size=(1, 3), strides=(1, 2), padding='same'),
+          tw.keras.layers.Dropout(self.hparams['dropout']),
+          # --- Second Convolutional Block ---
+          tw.keras.layers.Conv2D(self.hparams['n_filters'] * 2,
+                                 kernel_size=(1, self.hparams['s_filters']),
+                                 padding='same',
+                                 kernel_initializer=self.hparams['m_init']),
+          tw.keras.layers.BatchNormalization(),
+          self.hparams['activ_fn'],
+          tw.keras.layers.Conv2D(self.hparams['n_filters'] * 2,
+                                 kernel_size=(1, self.hparams['s_filters']),
+                                 padding='same',
+                                 kernel_initializer=self.hparams['m_init']),
+          tw.keras.layers.BatchNormalization(),
+          self.hparams['activ_fn'],
+          tw.keras.layers.MaxPool2D(pool_size=(1, 3), strides=(1, 2), padding='same'),
+          tw.keras.layers.Dropout(self.hparams['dropout']),
+          # --- Third Convolutional Block ---
+          tw.keras.layers.Conv2D(self.hparams['n_filters'] * 4,
+                                 kernel_size=(1, self.hparams['s_filters']),
+                                 padding='same',
+                                 kernel_initializer=self.hparams['m_init']),
+          tw.keras.layers.BatchNormalization(),
+          self.hparams['activ_fn'],
+          tw.keras.layers.Conv2D(self.hparams['n_filters'] * 4,
+                                 kernel_size=(1, self.hparams['s_filters']),
+                                 padding='same',
+                                 kernel_initializer=self.hparams['m_init']),
+          tw.keras.layers.BatchNormalization(),
+          self.hparams['activ_fn'],
+          tw.keras.layers.MaxPool2D(pool_size=(1, 3), strides=(1, 2), padding='same'),
+          tw.keras.layers.Dropout(self.hparams['dropout']),
+          # --- Flatten for Classification Task ---
+          tw.keras.layers.Flatten(),
+          # --- First FCN Layer ---
+          tw.keras.layers.Dense(self.hparams['n_units'],
+                                kernel_initializer=self.hparams['m_init']),
+          tw.keras.layers.BatchNormalization(),
+          self.hparams['activ_fn'],
+          tw.keras.layers.Dropout(self.hparams['dropout']),
+          # --- Second FCN Layer ---
+          tw.keras.layers.Dense(self.hparams['n_units'] * 2,
+                                kernel_initializer=self.hparams['m_init']),
+          tw.keras.layers.BatchNormalization(),
+          self.hparams['activ_fn'],
+          tw.keras.layers.Dropout(self.hparams['dropout']),
+          # --- Third FCN Layer ---
+          tw.keras.layers.Dense(self.hparams['n_units'] * 2,
+                                kernel_initializer=self.hparams['m_init']),
+          tw.keras.layers.BatchNormalization(),
+          self.hparams['activ_fn'],
+          tw.keras.layers.Dropout(self.hparams['dropout']),
+          # --- Fourth FCN Layer ---
+          tw.keras.layers.Dense(self.hparams['n_units'] * 2,
+                                kernel_initializer=self.hparams['m_init']),
+          tw.keras.layers.BatchNormalization(),
+          self.hparams['activ_fn'],
+          # --- Final FCN Prediction Layer ---
+          tw.keras.layers.Dense(20, 'softmax')
+        ], name='TCNN')
+
+        model_tcnn.compile(optimizer=self.hparams['optim'](),
+                          metrics=['categorical_accuracy'],
+                          loss='categorical_crossentropy')
+
+        return model_tcnn
+
+    def periodic_rate_decrementer(self, start: int, period: int, factor: float) -> Callable[[int, float], float]:
+        """Decrease the loss by a factor every period epochs starting from a point."""
+        def stabilize_lr(epoch: int, learn: float) -> float:
+            if epoch < start:
+                return learn
+            if (epoch - start) % period == 0:
+                return learn * factor
+            return learn
+        return stabilize_lr
+
+    def epoch_graph_callback(self) -> tw.keras.callbacks.Callback:
+        """Allow log output to be inspected using TensorBoard."""
+        return tw.keras.callbacks.TensorBoard(log_dir=self.logs_dir_,
+                                              write_graph=True)
+
+    def gpu_utiliz_callback(self) -> tw.keras.callbacks.Callback:
+        """Watch out for GPU memory / utilization constraints."""
+        return GpuStatsLogger(['/gpu:0'], memory_utilization=True, gpu_utilization=True)
+
+    def lr_updater_callback(self) -> tw.keras.callbacks.Callback:
+        return tw.keras.callbacks.LearningRateScheduler(verbose=1,
+          schedule=self.periodic_rate_decrementer(*self.hparams['lr_update']))
+
+    def fitting_callbacks(self) -> List[tw.keras.callbacks.Callback]:
+        return [
+          self.epoch_graph_callback(),
+          self.gpu_utiliz_callback(),
+          self.lr_updater_callback()
+        ]
--- projects/smart/src/modules/processing.py
+++ projects/smart/src/modules/processing.py
@@ -1,40 +1,153 @@
-from numpy.random import default_rng
-from tensorflow import Tensor
-import tensorflow as tw
+from __future__ import annotations
+import copy
+import pathlib as pb
+from typing import Tuple
 import pandas as ps
 import numpy as ny
+from numpy.random import default_rng
 
+class Dataset(object):
+    def __init__(self, path: pb.Path):
+        # Load the dataset "as is" without applying preprocessing
+        self.train_data,   \
+        self.train_labels, \
+        self.test_data = Dataset.load_dataset(path)
 
-def impute_missing_values(sample: Tensor, n_size: int) -> Tensor:
-    """Fill in the missing values (rows) by computing the ewm."""
-    sample_new = ps.DataFrame(columns=range(
-        sample.shape[1]), index=range(n_size), dtype=ny.float64)
+    def reshape(self, shape: Tuple[int, ...]) -> Dataset:
+        """Reshape the training & testing data."""
+        self.train_data = self.train_data.reshape(shape)
+        self.test_data = self.test_data.reshape(shape)
+        return self
 
-    # Generate indices to copy associated values
-    gen = default_rng()
-    sample_new_i = ny.sort(gen.choice(n_size, sample.shape[0], replace=False))
+    def fill_gaps(self) -> Dataset:
+        """Fill in interpolated elements in-between existing ones."""
+        self.train_data = Dataset.fill_missing_values(self.train_data, 150)
+        self.test_data = Dataset.fill_missing_values(self.test_data, 150)
+        self.train_labels = ny.array(self.train_labels)
+        return self
 
-    # Copy old values while keeping same order
-    sample_new.iloc[sample_new_i] = ps.DataFrame(sample.numpy())
+    def normalize(self, strategy: str, axis: Tuple[int, ...], norm: int = 2, eps=1e-10) -> Dataset:
+        """Normalizes the training and testing data using a custom strategy, over
+        the specified dimensions. Assumes that the data has fixed size.
+        Args:
+            strategy:
+            - standard
+            - min_max
+            - normalize
 
-    # Use interpolation to fill gaps and copy values where impossible
-    gaps_filled = sample_new.interpolate(axis='index').fillna(method='bfill')
-    return tw.convert_to_tensor(gaps_filled)
-
-
-def fill_missing_values(dataset: list[Tensor], n_size: int) -> Tensor:
-    """Receive a list of tensors of different sizes, fill in the missing values using
-      a strategy (ex. ewm) then return a tensor of tensors of the same size."""
-
-    for i, _ in enumerate(dataset):
-        if dataset[i].shape[0] == n_size:
-            continue
-        elif dataset[i].shape[0] < n_size:
-            dataset[i] = impute_missing_values(dataset[i], n_size)
+            axis: Tuple indicating the dimensions to compute over.
+            norm: Indicates the order (rank) for the normalization operation
+            eps: A small number added to avoid division by zero errors
+        Returns: The current dataset with the train_data and test_data normalized internally.
+        """
+        if strategy == 'standard':
+            m_train = self.train_data.mean(axis)
+            s_train = self.train_data.std(axis)
+            self.train_data = (self.train_data - m_train) / s_train
+            self.test_data = (self.test_data - m_train) / s_train
+        elif strategy == 'min_max':
+            mi_train = self.train_data.min(axis)
+            mx_train = self.train_data.max(axis)
+            self.train_data = (self.train_data - mi_train) / (mx_train - mi_train + eps)
+            mi_test = self.test_data.min(axis)
+            mx_test = self.test_data.max(axis)
+            self.test_data = (self.test_data - mi_test) / (mx_test - mi_test + eps)
+        elif strategy == 'normalize':
+            norm_train = ny.linalg.norm(self.train_data, ord=norm, axis=axis)
+            norm_train = ny.expand_dims(norm_train, axis=axis)
+            self.train_data /= norm_train
+            norm_test = ny.linalg.norm(self.test_data, ord=norm, axis=axis)
+            norm_test = ny.expand_dims(norm_test, axis=axis)
+            self.test_data /= norm_test
+        elif strategy is None:
+            return self
         else:
-            dataset[i] = dataset[i][:n_size]
+            raise Exception(f'Invalid normalize strategy argument: {strategy}')
+        return self
 
-    return tw.convert_to_tensor(dataset)
+    @staticmethod
+    def load_dataset(dataset_path: pb.Path) -> Tuple[list[ny.ndarray], list[ny.int64], list[ny.ndarray]]:
+        """Read the whole dataset in memory available from the base path:
+        Args:
+            path:
+                path/train/*.csv
+                path/train_labels.csv
+                path/test/*.csv
+        Returns:
+            A tuple containing three python lists:
+            - train_data
+            - train_labels
+            - test_data
+        """
+        # Read train labels raw and use them for lookup
+        train_labels_lookup = ps.read_csv(dataset_path / 'train_labels.csv')
+
+        # Fetch eatch train sample and do lookup for the corresponding label
+        train_data = []
+        train_labels = []
+        for sample_path in sorted((dataset_path / 'train').glob('*.csv')):
+            # Lookup the label
+            train_label = train_labels_lookup[train_labels_lookup['id'] == int(
+                sample_path.stem)]
+            train_labels.append(train_label['class'].item())
+
+            # Read the sample and convert it to a tf compatible format
+            train_entry = ps.read_csv(
+                sample_path, header=None, names=['x', 'y', 'z'])
+            train_data.append(train_entry.to_numpy())
+        print('Number of training samples: ', len(train_data))
+        print('Shape of a training sample: ', train_data[0].shape)
+
+        # Read test samples
+        test_data = []
+        for sample_path in sorted((dataset_path / 'test').glob('*.csv')):
+            test_entry = ps.read_csv(
+                sample_path, header=None, names=['x', 'y', 'z'])
+            test_data.append(test_entry.to_numpy())
+        print('Number of testing samples: ', len(test_data))
+        print('Shape of a testing sample: ', test_data[0].shape)
+        return train_data, train_labels, test_data
+
+    @staticmethod
+    def impute_missing_values(sample: ny.ndarray, n_size: int) -> ny.ndarray:
+        """Fill in the missing values (rows) by computing the ewm."""
+        sample_new = ps.DataFrame(columns=range(sample.shape[1]),
+                                  index=range(n_size),
+                                  dtype=ny.float64)
+
+        # Generate indices to copy associated values
+        gen = default_rng()
+
+        # To avoid filling the start and end of a sequence with repeated copies
+        # of the same element, lock in the head and the tail and allow intermediary
+        # interpolated values to occurr.
+        sample_new_i = ny.sort(gen.choice(n_size - 2, sample.shape[0] - 2, replace=False) + 1)
+
+        # Lock in the head and the tail
+        sample_new.iloc[0], sample_new.iloc[-1] = sample[0], sample[-1]
+
+        # Copy old values while keeping same order
+        sample_new.iloc[sample_new_i] = ps.DataFrame(sample[1:-1])
+
+        # Use interpolation to fill gaps and copy values where impossible
+        gaps_filled = sample_new.interpolate(axis='index').fillna(method='bfill')
+        return gaps_filled.to_numpy().astype(ny.float32)
+
+    @staticmethod
+    def fill_missing_values(dataset: list[ny.ndarray], n_size: int) -> ny.ndarray:
+        """Receive a list of tensors of different sizes, fill in the missing values using
+        a strategy (ex. ewm) then return a tensor of tensors of the same size."""
+        dataset = copy.deepcopy(dataset)
+
+        for i, _ in enumerate(dataset):
+            if dataset[i].shape[0] == n_size:
+                continue
+            elif dataset[i].shape[0] < n_size:
+                dataset[i] = Dataset.impute_missing_values(dataset[i], n_size)
+            else:
+                dataset[i] = dataset[i][:n_size]
+
+        return ny.array(dataset, dtype=ny.float32)
 
 
 if __name__ == '__main__':
