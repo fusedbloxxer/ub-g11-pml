89cd828760f5a935ea77b81c4813503f46fd1986
fusedbloxxer <voidwarden@yahoo.com>
2022-11-30 02:17:42 +0200

    Add outlier removal methods (wip) - submissions 15-17

--- projects/smart/src/modules/processing.py
+++ projects/smart/src/modules/processing.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 import copy
 import pathlib as pb
-from typing import Tuple
+from typing import Tuple, List, Optional
 import pandas as ps
 import numpy as ny
 from numpy.random import default_rng
@@ -19,11 +19,12 @@ class Dataset(object):
         self.test_data = self.test_data.reshape(shape)
         return self
 
-    def fill_gaps(self) -> Dataset:
+    def fill_gaps(self, n_size: int = 150, min_limit: Optional[int] = 75) -> Dataset:
         """Fill in interpolated elements in-between existing ones."""
-        self.train_data = Dataset.fill_missing_values(self.train_data, 150)
-        self.test_data = Dataset.fill_missing_values(self.test_data, 150)
-        self.train_labels = ny.array(self.train_labels)
+        self.train_data, deleted_i = Dataset.fill_missing_values(self.train_data, n_size, min_limit)
+        self.test_data, _ = Dataset.fill_missing_values(self.test_data, n_size)
+        self.train_labels = ny.array([l for i, l in enumerate(self.train_labels)
+                                        if i not in deleted_i])
         return self
 
     def normalize(self, strategy: str, axis: Tuple[int, ...], norm: int = 2, eps=1e-10) -> Dataset:
@@ -63,6 +64,104 @@ class Dataset(object):
             return self
         else:
             raise Exception(f'Invalid normalize strategy argument: {strategy}')
+        return self
+
+    def to_pandas(self) -> ps.DataFrame:
+        """Concatenate all training data samples into a single table, in order
+        to perform statistics in a more flexible manner."""
+        records = []
+        for i, (data, label) in enumerate(zip(self.train_data, self.train_labels)):
+            # Extract the column individually and append label and identifier
+            x, y, z = data[:, 0], data[:, 1], data[:, 1]
+            target = ny.full_like(x, label, dtype=ny.int64)
+            id = ny.full_like(x, i, dtype=ny.int64)
+
+            # Aggregate the values
+            sample = ps.DataFrame({
+                'id': id,
+                'x': x,
+                'y': y,
+                'z': z,
+                'label': target,
+            })
+
+            # Save to previously seen records
+            records.append(sample)
+        return ps.concat(records, ignore_index=True)
+
+    def from_pandas(self, data: ps.DataFrame) -> Tuple[List[ny.ndarray], List[int]]:
+        """Given a DataFrame that contains all info (see to_pandas), restore it
+        to the original form as an array of arrays (samples) and an array (labels)."""
+        data_labels, data_records = [], []
+        for i in ps.unique(data['id']):
+            # Extract all rows for the test entry
+            data_sample = data[data['id'] == i].reset_index()
+
+            # Extract the values as a ny array and item
+            data_records.append(ny.array(data_sample[['x', 'y', 'z']]))
+            data_labels.append(data_sample['label'].iloc[0].item())
+        return data_records, data_labels
+
+    def remove_outliers(self, by: str, factor: float = +15e-1) -> Dataset:
+        """Remove outliers for each feature (x, y, z) by calculating the IQR,
+        and using a factor to eliminate them.
+
+        Args:
+            by (str): If 'global' then compute the IQR over the entire dataset,
+            otherwise if 'class' then compute the IQR for each class.
+        """
+        # Transform the dataset to easily compute stats
+        train_data = self.to_pandas()
+
+        if by == 'global':
+            # Compute the boundary used during the elimination process
+            qu_1 = train_data.quantile(q=25e-2)[['x', 'y', 'z']]
+            qu_3 = train_data.quantile(q=75e-2)[['x', 'y', 'z']]
+            iqr = ny.abs(qu_3 - qu_1)
+            iqr_l = qu_1 - iqr * factor
+            iqr_u = qu_3 + iqr * factor
+
+            # Remove the outliers using IQR
+            no_outliers = train_data[
+                (train_data['x'] >= iqr_l['x']) & (train_data['x'] <= iqr_u['x']) &
+                (train_data['y'] >= iqr_l['y']) & (train_data['y'] <= iqr_u['y']) &
+                (train_data['z'] >= iqr_l['z']) & (train_data['z'] <= iqr_u['z'])
+            ]
+        elif by == 'class':
+            def boundary(subset: ps.DataFrame, factor = 1.5):
+                """Compute IQRs for each subset of samples of the same class."""
+                qu_1 = subset.quantile(q=25e-2)[['x', 'y', 'z']]
+                qu_3 = subset.quantile(q=75e-2)[['x', 'y', 'z']]
+                iqr = ny.abs(qu_3 - qu_1)
+                iqr_l = qu_1 - iqr * factor
+                iqr_u = qu_3 + iqr * factor
+                return ps.DataFrame({
+                    'x': ps.Series([iqr_l['x'], iqr_u['x']], index=['iqr_l', 'iqr_u']),
+                    'y': ps.Series([iqr_l['y'], iqr_u['y']], index=['iqr_l', 'iqr_u']),
+                    'z': ps.Series([iqr_l['z'], iqr_u['z']], index=['iqr_l', 'iqr_u']),
+                })
+
+            # Group entries by class
+            thresholds = train_data \
+                .groupby('label', group_keys=True) \
+                .apply(boundary)
+
+            # Remove outliers from each class
+            subsets = []
+            for i in range(1, 21):
+                subsets.append(train_data[
+                    (train_data['label'] == i) &
+                    (train_data['x'] >= thresholds.loc[i].loc['iqr_l', 'x']) &
+                    (train_data['x'] <= thresholds.loc[i].loc['iqr_u', 'x']) &
+                    (train_data['y'] >= thresholds.loc[i].loc['iqr_l', 'y']) &
+                    (train_data['y'] <= thresholds.loc[i].loc['iqr_u', 'y']) &
+                    (train_data['z'] >= thresholds.loc[i].loc['iqr_l', 'z']) &
+                    (train_data['z'] <= thresholds.loc[i].loc['iqr_u', 'z'])
+                ])
+            no_outliers = ps.concat(subsets)
+
+        # Transform back to the initial representation
+        self.train_data, self.train_labels = self.from_pandas(no_outliers)
         return self
 
     @staticmethod
@@ -134,20 +233,37 @@ class Dataset(object):
         return gaps_filled.to_numpy().astype(ny.float32)
 
     @staticmethod
-    def fill_missing_values(dataset: list[ny.ndarray], n_size: int) -> ny.ndarray:
+    def fill_missing_values(dataset: list[ny.ndarray], n_size: int, min_limit: Optional[int] = None) -> ny.ndarray:
         """Receive a list of tensors of different sizes, fill in the missing values using
-        a strategy (ex. ewm) then return a tensor of tensors of the same size."""
+        a strategy (ex. ewm) then return a tensor of tensors of the same size.
+        Args:
+            n_size: Specifies the number of rows expected at the output for each sample
+            in the dataset.
+            min_limit: Represents a lower-limit threshold which is used to
+            discard samples that have too many missing datapoints.
+        Returns:
+            An array containing the dataset with the added/reduced values and
+            an array containing the indices for the entries that were deleted.
+        """
         dataset = copy.deepcopy(dataset)
 
+        mark_for_deletion = []
         for i, _ in enumerate(dataset):
             if dataset[i].shape[0] == n_size:
                 continue
+            elif min_limit is not None and dataset[i].shape[0] < min_limit:
+                mark_for_deletion.append(i)
             elif dataset[i].shape[0] < n_size:
                 dataset[i] = Dataset.impute_missing_values(dataset[i], n_size)
             else:
                 dataset[i] = dataset[i][:n_size]
 
-        return ny.array(dataset, dtype=ny.float32)
+        # When an element is deleted from the list decr the i to accomodate
+        # for the shifted values
+        for s, i in enumerate(mark_for_deletion):
+            del dataset[i - s]
+
+        return ny.array(dataset, dtype=ny.float32), mark_for_deletion
 
 
 if __name__ == '__main__':
